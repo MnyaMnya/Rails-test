bdd35198295f1be9fd40da19cc99fc05b9a9f869
MnyaMnya <81231166+MnyaMnya@users.noreply.github.com>
2024-07-25 19:00:58 +0700

    change bind to 0.0.0.0

--- config/puma.rb
+++ config/puma.rb
@@ -24,7 +24,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #port ENV.fetch("PORT") { 3000 }
-bind        'tcp://192.168.100.3:3000'
+bind        'tcp://0.0.0.0:3000'
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
