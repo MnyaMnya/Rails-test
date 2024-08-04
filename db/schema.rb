b762c7b021fc592ddee50fd8f7df6f8a17627c3e
MnyaMnya <81231166+MnyaMnya@users.noreply.github.com>
2024-08-04 19:46:50 +0700

    add friend list and change index view to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_28_124539) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_04_104558) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_28_124539) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "git"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
