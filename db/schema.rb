a1624f37e2c096286acdf18052afed1499926417
MnyaMnya <i60889958@gmail.com>
2024-08-17 20:08:57 +0700

    Update rails version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_09_120943) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_17_125618) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
