2b0c7e01fcd51806052cbf245248ac09d6cd7f84
MnyaMnya <i60889958@gmail.com>
2024-09-12 11:25:26 +0700

    force username and email to be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
 	has_secure_password
-
-	validates :email, presence: true
+  
+  validates :username, uniqueness: { message: "Username Already Taken", case_sensitive: false }
+	validates :email, presence: true, uniqueness: { message: "Email Already Used", case_sensitive: false }
 	normalizes :email, with: -> (email) {email.strip.downcase}
 
 	generates_token_for :password_reset, expires_in: 15.minutes do
